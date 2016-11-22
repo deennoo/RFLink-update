d69bf79f6589f23091e4d981a77708b639ac681b
deennoo <deennoo33@gmail.com>
2016-11-22 13:02:23 +0100

    update with automatique get -serial -name -idx active and disactive rflink on domoticz
    update with automatique get -serial -name -idx active and disactive rflink on domoticz

--- update_RFLink.sh
+++ update_RFLink.sh
@@ -6,44 +6,71 @@ NC='\033[0m' # No Color
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-#WELCOME MESSAGE
+# #WELCOME MESSAGE
 curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20STARTING' > /dev/null
-echo -e "${YELLOW}Welcome to RFLink Update Script, please unactive RFLink on Domoticz Hardware Panel,${NC} ${RED}type ok + enter when done${NC}"
-read ok
-echo -e "${YELLOW}OK Done ? Lets' GO !${NC}"
+echo -e "${YELLOW}Welcome to RFLink Update Script${NC}"
+# read ok
+# echo -e "${YELLOW}OK Done ? Lets' GO !${NC}"
 
-#creatE RFLink update dir on tmp
+# #Create RFLink update dir on tmp
 echo -e "${YELLOW}Create RFLink dir on /tmp${NC}"
 sudo mkdir /tmp/RFLink &> /dev/null
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20CREATE%20RFLINK%20DIR%20ON%20TMP' > /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20CREATE%20RFLINK%20DIR%20ON%20TMP'   > /dev/null
 
-#download last fw, and copy to /tmp/RFLink
+# #download last fw, and copy to /tmp/RFLink > /dev/null
 echo -e "${YELLOW}DOWNLOADING LAST RFLINK FW${NC}"
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20DOWNLOADING%20LAST%20RFLINK%20FW' > /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20DOWNLOADING%20LAST%20RFLINK%20FW'  > /dev/null
 cd /tmp/RFLink
 sudo rm -f /tmp/RFLink/RFLink.cpp.hex
 sudo wget http://www.nemcon.nl/blog2/fw/44/RFLink.cpp.hex &> /dev/null
 
-#check and install avrdude, if avrdudue is already here do nothing, if not install it
+# #check and install avrdude & jq, if avrdudue & jq are already here do nothing, if not install it
 echo -e "${YELLOW}INSTALLING DEPENDENCIE${NC}"
-sudo apt-get install avrdude -y
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20INSTALLING%20DEPENDENCY' > /dev/null
+sudo apt-get install avrdude -y  > /dev/null
+sudo apt-get install jq -y  > /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20INSTALLING%20DEPENDENCY'  > /dev/null
 
-#ask to user which ttyusb is use for rflink or take it from domoticz db ?
-echo -e "${YELLOW}Please add your RFLink adress ${NC}${RED}ex :  /dev/ttyUSB0 ? /!\ don't forget to unactive RFLink on Domoticz hardware panel${NC}"
-read usbgateway
-echo -e "${YELLOW}We will use ${RED}$usbgateway${NC}${YELLOW} as communication port${NC}"
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20COMMUNICATION%20PORT%20SET' > /dev/null
+#get RFLink IDX hardware
+idx=$(curl -s http://127.0.0.1:8080/json.htm?type=hardware | jq '.result[] | select(.Type == 46)'| jq '.idx?')
+IDX="${idx%\"}"
+IDX="${IDX#\"}"
+echo -e "${YELLOW}Your RFLink Hardware IDX is == ${NC}${RED}$IDX${NC}"
 
+#get RFLink serial port
+SerialPort=$(curl -s http://127.0.0.1:8080/json.htm?type=hardware | jq '.result[] | select(.Type == 46)'| jq '.SerialPort?')
+SERIAL="${SerialPort%\"}"
+SERIAL="${SERIAL#\"}"
+echo -e "${YELLOW}Your RFLink Hardware serial port is == ${NC}${RED}$SERIAL${NC}"
+
+#get RFLink hardwareName
+Name=$(curl -s http://127.0.0.1:8080/json.htm?type=hardware | jq '.result[] | select(.Type == 46)'| jq '.Name?')
+NAME="${Name%\"}"
+NAME="${NAME#\"}"
+echo -e "${YELLOW}Your RFLink Hardware Name is == ${NC}${RED}$NAME${NC}"
+
+#get RFLink version
+Version=$(curl -s http://127.0.0.1:8080/json.htm?type=hardware | jq '.result[] | select(.Type == 46)'| jq '.version?')
+VERSION="${Version%\"}"
+VERSION="${VERSION#\"}"
+echo -e "${YELLOW}Your RFLink version is == ${NC}${RED}$VERSION${NC}"
+
+#disable RFLink hardware
+echo -e "${YELLOW}Disabling RFLink Hardware${NC}"
+curl --silent -s -i -H  "Accept: application/json" "http://127.0.0.1:8080/json.htm?type=command&param=updatehardware&htype=46&port=$SERIAL&extra=&name=$NAME&enabled=false&idx=$IDX&datatimeout=0&Mode1=0&Mode2=0&Mode3=0&Mode4=0&Mode5=0&Mode6=0"  > /dev/null
 
 #update rflink with last FW
 echo -e "${YELLOW}UPDATING YOUR RFLINK ${NC}${RED} CAN TAKES TIME BE PATIENT PLEASE ${NC}"
-sudo /usr/bin/avrdude -v -v -v -p atmega2560 -c wiring -D -P $usbgateway -b 115200 -U flash:w:/tmp/RFLink/RFLink.cpp.hex:i &> /dev/null
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20FINISHED%20---%20PLEASE%20REACTIVATE%20YOUR%20RFLINK%20ON%20HARDWARE%20PANEL' > /dev/null
+sudo /usr/bin/avrdude -v -v -v -p atmega2560 -c wiring -D -P $SERIAL -b 115200 -U flash:w:/tmp/RFLink/RFLink.cpp.hex:i  &> /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20IN%20PROGRES%20---'  > /dev/null
+
+#enable RFLink hardware
+echo -e "${YELLOW}Enabling RFLink Hardware${NC}"
+curl --silent -s -i -H  "Accept: application/json" "http://127.0.0.1:8080/json.htm?type=command&param=updatehardware&htype=46&port=$SERIAL&extra=&name=$NAME&enabled=true&idx=$IDX&datatimeout=0&Mode1=0&Mode2=0&Mode3=0&Mode4=0&Mode5=0&Mode6=0"  > /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20FINISHED%20---'  > /dev/null
 
 #print thx to users and don't forget to reactivate RFLink on Domoticz
 echo -e "${YELLOW}Update Done, Thanks using RFLink${NC}"
-echo -e "${YELLOW}Please Active your RFlink on Domoticz Hardware Panel${NC}"
+
 echo -e "${RED} _____    _____   _       _   __   _   _   _   ${NC}" 
 echo -e "${RED}|  _  \  |  ___| | |     | | |  \ | | | | / /  ${NC}" 
 echo -e "${RED}| |_| |  | |__   | |     | | |   \| | | |/ /   ${NC}" 
