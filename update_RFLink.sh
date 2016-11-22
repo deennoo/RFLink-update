95a1d1e3e5e3b8e391611cd483d112608a880f91
deennoo <deennoo33@gmail.com>
2016-11-22 13:16:13 +0100

    Update update_RFLink.sh

--- update_RFLink.sh
+++ update_RFLink.sh
@@ -12,18 +12,6 @@ echo -e "${YELLOW}Welcome to RFLink Update Script${NC}"
 # read ok
 # echo -e "${YELLOW}OK Done ? Lets' GO !${NC}"
 
-# #Create RFLink update dir on tmp
-echo -e "${YELLOW}Create RFLink dir on /tmp${NC}"
-sudo mkdir /tmp/RFLink &> /dev/null
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20CREATE%20RFLINK%20DIR%20ON%20TMP'   > /dev/null
-
-# #download last fw, and copy to /tmp/RFLink > /dev/null
-echo -e "${YELLOW}DOWNLOADING LAST RFLINK FW${NC}"
-curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20DOWNLOADING%20LAST%20RFLINK%20FW'  > /dev/null
-cd /tmp/RFLink
-sudo rm -f /tmp/RFLink/RFLink.cpp.hex
-sudo wget http://www.nemcon.nl/blog2/fw/44/RFLink.cpp.hex &> /dev/null
-
 # #check and install avrdude & jq, if avrdudue & jq are already here do nothing, if not install it
 echo -e "${YELLOW}INSTALLING DEPENDENCIE${NC}"
 sudo apt-get install avrdude -y  > /dev/null
@@ -54,14 +42,33 @@ VERSION="${Version%\"}"
 VERSION="${VERSION#\"}"
 echo -e "${YELLOW}Your RFLink version is == ${NC}${RED}$VERSION${NC}"
 
+#Create RFLink update dir on tmp
+echo -e "${YELLOW}Create RFLink dir on /tmp${NC}"
+sudo mkdir /tmp/RFLink &> /dev/null
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20CREATE%20RFLINK%20DIR%20ON%20TMP'   > /dev/null
+
+#check for a new FW version
+#to be done
+echo -e "${YELLOW}Check for a new RFLink FW${NC}"
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20CHECK%20FOR%20NEW%20RFLINK%20FW'   > /dev/null
+
+
+# #download last fw, and copy to /tmp/RFLink > /dev/null
+echo -e "${YELLOW}DOWNLOADING LAST RFLINK FW${NC}"
+curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20DOWNLOADING%20LAST%20RFLINK%20FW'  > /dev/null
+cd /tmp/RFLink
+sudo rm -f /tmp/RFLink/RFLink.cpp.hex
+sudo wget http://www.nemcon.nl/blog2/fw/44/RFLink.cpp.hex &> /dev/null
+
 #disable RFLink hardware
 echo -e "${YELLOW}Disabling RFLink Hardware${NC}"
 curl --silent -s -i -H  "Accept: application/json" "http://127.0.0.1:8080/json.htm?type=command&param=updatehardware&htype=46&port=$SERIAL&extra=&name=$NAME&enabled=false&idx=$IDX&datatimeout=0&Mode1=0&Mode2=0&Mode3=0&Mode4=0&Mode5=0&Mode6=0"  > /dev/null
 
 #update rflink with last FW
 echo -e "${YELLOW}UPDATING YOUR RFLINK ${NC}${RED} CAN TAKES TIME BE PATIENT PLEASE ${NC}"
-sudo /usr/bin/avrdude -v -v -v -p atmega2560 -c wiring -D -P $SERIAL -b 115200 -U flash:w:/tmp/RFLink/RFLink.cpp.hex:i  &> /dev/null
 curl -s 'http://127.0.0.1:8080/json.htm?type=command&param=addlogmessage&message=---%20RFLINK%20UPDATE%20---%20IN%20PROGRES%20---'  > /dev/null
+sudo /usr/bin/avrdude -v -v -v -p atmega2560 -c wiring -D -P $SERIAL -b 115200 -U flash:w:/tmp/RFLink/RFLink.cpp.hex:i  &> /dev/null
+
 
 #enable RFLink hardware
 echo -e "${YELLOW}Enabling RFLink Hardware${NC}"
